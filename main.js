92b58c541ebfe9a2fcfce0accacecd656acb4e8f
Cloudiz1 <vincentni2007@gmail.com>
2024-05-23 06:56:57 -0400

    changed side bar

--- main.js
+++ main.js
@@ -4,29 +4,15 @@ let close_button = document.getElementsByClassName("close_button_container")[0];
 let main_content = document.getElementsByClassName("main_content")[0];
 
 function open_sidebar() {
-    main_content.style.filter = "blur(.3rem)"
-    main_content.style.opacity = .3;
-    open_button.style.opacity = 0;
-    setTimeout(() => {
-        sidebar.style.left = "0px";
-        sidebar.style.opacity = 1;
-        close_button.style.transform = "scale(1)";
-        close_button.style.opacity = 1;
-    }, 50);
-
-
+    sidebar.style.left = "0px";
+    main_content.style.left = "20vw";
+    open_button.style.opacity = "0";
 }
 
 function close_sidebar() {
-    main_content.style.filter = "blur(0)"
-    main_content.style.opacity = 1
     sidebar.style.left = "-410px";
-    sidebar.style.opacity = 0;
-    close_button.style.transform = "scale(0)";
-    close_button.style.opacity = 0;
+    main_content.style.left = "15vw";
     setTimeout(() => {
-        open_button.style.opacity = 1;
-    }, 150);
-
-
+        open_button.style.opacity = "100";
+    }, 200)
 }
